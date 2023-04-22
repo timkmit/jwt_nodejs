2cfe2ba99099a3d2063b4fc23499b5ebe17414bf
Timur <timengalytchev@mail.ru>
2023-04-22 21:33:29 +0300

    made validation with express-validator

--- authController.js
+++ authController.js
@@ -1,10 +1,15 @@
 const User = require('./models/Users')
 const Role = require('./models/Role')
 const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator')
 
 class authController {
     async registration(req, res){
         try {
+            const errors = validationResult(req)
+            if(!errors.isEmpty()){
+                return res.status(400).json({message: 'Ошибка при регистрации', errors})
+            }
             const { username, password} = req.body
             const candidate = await User.findOne({username})
             if(candidate){
--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
+const {check} = require('express-validator')
 
-router.post('/registration', controller.registration)
+router.post('/registration',
+[check('username', "Имя пользователя не может быть пустым").isEmpty(),
+check('password', "Пароль должен содержать более 4 и меньше 15 символов").isLength({min: 4, max: 15})]
+, controller.registration)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
